88afd50a6ea4bcc80067ccaeadbf2e570a04f5e9
Ron DuPlain <ron.duplain@gmail.com>
2015-07-03 14:12:54 -0400

    Move twitter factory function into class method.

--- src/birding/twitter_api.py
+++ src/birding/twitter_api.py
@@ -2,42 +2,46 @@
 
 import os
 
-from twitter.api import Twitter
+from twitter.api import Twitter as BaseTwitter
 from twitter.cmdline import CONSUMER_KEY, CONSUMER_SECRET
 from twitter.oauth import OAuth, read_token_file
 
 
-def build_twitter():
-    """Get an object bound to the Twitter API using your own credentials.
+class Twitter(BaseTwitter):
+    @classmethod
+    def from_oauth_file(cls, filepath=None):
+        """Get an object bound to the Twitter API using your own credentials.
 
-    The `twitter` library ships with a `twitter` command that uses PIN
-    OAuth. Generate your own OAuth credentials by running `twitter` from the
-    shell, which will open a browser window to authenticate you. Once
-    successfully run, even just one time, you will have a credential file at
-    ~/.twitter_oauth.
+        The `twitter` library ships with a `twitter` command that uses PIN
+        OAuth. Generate your own OAuth credentials by running `twitter` from
+        the shell, which will open a browser window to authenticate you. Once
+        successfully run, even just one time, you will have a credential file
+        at ~/.twitter_oauth.
 
-    This factory function reuses your credential file to get a `Twitter`
-    object. (Really, this code is just lifted from the `twitter.cmdline` module
-    to minimize OAuth dancing.)
-    """
-    oauth_filename = (
-        os.environ.get('HOME', os.environ.get('USERPROFILE', ''))
-        + os.sep + '.twitter_oauth')
-    oauth_token, oauth_token_secret = read_token_file(oauth_filename)
+        This factory function reuses your credential file to get a `Twitter`
+        object. (Really, this code is just lifted from the `twitter.cmdline`
+        module to minimize OAuth dancing.)
+        """
+        if filepath is None:
+            # Use default OAuth filepath from `twitter` command-line program.
+            home = os.environ.get('HOME', os.environ.get('USERPROFILE', ''))
+            filepath = os.path.join(home, '.twitter_oauth')
 
-    twitter = Twitter(
-        auth=OAuth(
-            oauth_token, oauth_token_secret, CONSUMER_KEY, CONSUMER_SECRET),
-        api_version='1.1',
-        domain='api.twitter.com')
+        oauth_token, oauth_token_secret = read_token_file(filepath)
 
-    return twitter
+        twitter = cls(
+            auth=OAuth(
+                oauth_token, oauth_token_secret, CONSUMER_KEY, CONSUMER_SECRET),
+            api_version='1.1',
+            domain='api.twitter.com')
+
+        return twitter
 
 
 def main():
     """Do the default action of `twitter` command."""
     from twitter.cmdline import Action, OPTIONS
-    twitter = build_twitter()
+    twitter = Twitter.from_oauth_file()
     Action()(twitter, OPTIONS)
 
 
