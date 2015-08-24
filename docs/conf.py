75a5cb8175c1880939649c4fc9049bcc4734c331
Ron DuPlain <ron.duplain@gmail.com>
2015-08-24 17:52:44 -0400

    Add intersphinx configuration.
    Example links:
    
        :py:func:`textwrap.dedent`
        :py:class:`streamparse.storm.bolt.Bolt`
        :py:class:`pykafka.balancedconsumer.BalancedConsumer`
    
    To have the resulting reference remove the namespace, add '~':
    
        :py:class:`~pykafka.balancedconsumer.BalancedConsumer`

--- docs/conf.py
+++ docs/conf.py
@@ -360,6 +360,8 @@ epub_exclude_files = ['search.html']
 # If false, no index is generated.
 #epub_use_index = True
 
-
-# Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/dev', None),
+    'pykafka': ('http://pykafka.readthedocs.org/en/stable/', None),
+    'streamparse': ('http://streamparse.readthedocs.org/en/stable/', None),
+}
