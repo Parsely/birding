ecde3e00de3782024f0dfbab7f2d7912ab62342b
Ron DuPlain <ron.duplain@gmail.com>
2015-08-30 12:11:53 -0400

    Remove unnecessary objects from root namespace.

--- src/birding/__init__.py
+++ src/birding/__init__.py
@@ -3,15 +3,11 @@ from __future__ import absolute_import, print_function
 import logging
 
 from . import bolt, config, follow, search, spout, twitter_api
-from .search import SearchManager
-from .twitter_api import Twitter
 from .version import VERSION, __version__
 from .version import __doc__ as __license__
 
 
 __all__ = [
-    'SearchManager',
-    'Twitter',
     'VERSION',
     '__license__',
     '__version__',
