7fc2f1b7282304a9c7677e3860098037219ac2f9
Ron DuPlain <ron.duplain@gmail.com>
2015-08-31 01:18:13 -0400

    Release birding v0.1.

--- src/birding/version.py
+++ src/birding/version.py
@@ -14,5 +14,5 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-__version__ = '0.0.0'
+__version__ = '0.1'
 VERSION = tuple(int(x) for x in __version__.split('.'))
